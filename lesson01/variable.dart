53a22be48e4b4a57b12fcbb13b55beb519d47a0d
MinSang22Kim <powerminsang9901@gmail.com>
2025-05-07 19:43:56 +0900

    feat: 변수 타입 개념 실습

---
+++ lesson01/variable.dart
@@ -0,0 +1,17 @@
+void main(){
+  var name = '민상';
+  String name2 = '민상';
+  name = 'minsang';
+
+  print(name);
+  print(name2);
+
+  var age;
+  dynamic age2;
+  age=20;
+  age2=20;
+  age2='30';
+  
+  print(age);
+  print(age2);
+}
